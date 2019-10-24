7eff0f4f825d8b09b293c338e730baf5e9388d73
Michał Mrozek <Michsior14@users.noreply.github.com>
2019-10-24 13:03:50 +0200

    use wvvmp version of electron

--- main.js
+++ main.js
@@ -16,15 +16,12 @@ var options = {
     fastQuit: false,
     honest: false,
     conceal: false,
-    // flash is needed to actually play music without HTML5 DRM components
-    flash: true,
-    // we don't provide flash, due to licensing, but, you can find it inside ~/.config/google-chrome/PepperFlash/[version]/libpepflashplayer.so
-    flashPath: "./libpepflashplayer.so",
     zoom: 1.0,
     singleInstance: true,
     clearCache: false,
     tray: true,
     verbose: true,
+    electronVersion: "5.0.11-wvvmp",
     inject: ["./custom.js"],
     globalShortcuts: "shortcuts.json",
 };
@@ -34,27 +31,14 @@ nativefier(options, function(error, appPath) {
         console.log(error.red);
         return;
     }
-    fs.copyFile('./libpepflashplayer.so', './'+appPath+'/libpepflashplayer.so', function(error) {
-        if(error) {
-            if(error.code=='ENOENT'){
-                console.log("Could not copy libpepflashplayer.so : Is libpepflashplayer.so in the current directory?".bold.red);
-            }
-            else {
-                throw error;
-            }
-            return;
-        }
-        else {
-            // we now need to ensure that nativefier.json is set right (sometimes the name option doesn't pass correctly)
-            var rawdata = fs.readFileSync(appPath+'/resources/app/nativefier.json');  
-            var packageData = JSON.parse(rawdata);
-            packageData.name = "TIDAL";
-            // parts of our README ask people to change this file, so indenting it is polite
-            var to_write = JSON.stringify(packageData, null, 4); 
-            fs.writeFile(appPath+'/resources/app/nativefier.json', to_write, function(error) {
-                if(error) throw error;
-            });
-            console.log(("TIDAL is ready to start. cd to \""+appPath+"\", and run ./tidal").bold.green);
-        }
+    // we now need to ensure that nativefier.json is set right (sometimes the name option doesn't pass correctly)
+    var rawdata = fs.readFileSync(appPath+'/resources/app/nativefier.json');  
+    var packageData = JSON.parse(rawdata);
+    packageData.name = "TIDAL";
+    // parts of our README ask people to change this file, so indenting it is polite
+    var to_write = JSON.stringify(packageData, null, 4); 
+    fs.writeFile(appPath+'/resources/app/nativefier.json', to_write, function(error) {
+        if(error) throw error;
     });
+    console.log(("TIDAL is ready to start. cd to \""+appPath+"\", and run ./tidal").bold.green);
 });
