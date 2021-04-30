2927c0d584dfedbca82eb57a9a897dc376732e85
Daniel Fuchs <dfuchs@multamedio.de>
2021-04-30 13:07:00 +0200

    Dependencies updated
    - use nativefier from own repository to allow login in application

--- custom.js
+++ custom.js
@@ -1,147 +1,140 @@
-var options = {
-    "enable_notifications": true
-};
+let options = { "enable_notifications" : true };
 
 function log(data) {
-    document.getElementById("log").innerHTML = document.getElementById("log").innerHTML + "\n" + data;
+  document.getElementById("log").innerHTML = document.getElementById("log").innerHTML + "\n" + data;
 }
 
-function filterButtons(dataTestTag) {
-    // tidal has all it's playback buttons (apart from pause) begin with this
-    var buttons = document.querySelectorAll("[class^=whiteIconButton]");
-    for (var i = 0; i < buttons.length; i++) {
-        // tidal stores the purpose of a button in the data-test attribute
-        if (buttons[i].attributes['data-test'].value == dataTestTag) {
-            return buttons[i];
-        }
+function filterButtons(dataTestTag, method) {
+  // tidal has all it's playback buttons (apart from pause) begin with this
+  for (let button of document.querySelectorAll("[class^=whiteIconButton]")) {
+    // tidal stores the purpose of a button in the data-test attribute
+    if (button.attributes['data-test'].value === dataTestTag) {
+      try {
+        button[method]();
+      } catch (e) {
+        console.error(`${e.name}: ${e.message}`);
+      }
     }
-    return null; // only happens if we don't find a button
+  }
+
+  return null; // only happens if we don't find a button
 }
+
 function previousMedia() {
-    can_run = true;
-    if (window.lastKeyLast == undefined) {
-        // do nothing, this is to make sure we don't do the next two
-    } else if ((Date.now() - window.lastKeyLast) <= 200) {
-        can_run = false;
-    }
-    if (can_run) {
-        filterButtons("previous").click(); // filterButtons returns a dom object of the button we want to click
-        window.lastKeyLast = Date.now();
-    }
+  if (window.lastKeyLast === undefined || !(Date.now() - window.lastKeyLast) <= 200) {
+    filterButtons("previous", "click"); // filterButtons returns a dom object of the button we want to click
+    window.lastKeyLast = Date.now();
+  }
 }
 
 function nextMedia() {
-    can_run = true;
-    if (window.nextKeyLast == undefined) {
-        // do nothing, this is to make sure we don't do the next two
-    } else if ((Date.now() - window.nextKeyLast) <= 200) {
-        can_run = false;
-    }
-    if (can_run) {
-        filterButtons("next").click();
-        window.nextKeyLast = Date.now();
-    }
+  if (window.nextKeyLast === undefined || !(Date.now() - window.nextKeyLast) <= 200) {
+    filterButtons("next", "click");
+    window.nextKeyLast = Date.now();
+  }
 }
 
-
 function isPlaying() {
-    var pause = document.querySelectorAll("[class^=playbackToggle]")[0]; // the data tag of the play/pause button is only pause *if* the song is playing
-    if (pause.attributes['data-test'].value == "pause") { // if we can pause the song, it must be playing
-        return true;
-    }
-    return false;
+  let pause = document.querySelectorAll("[class^=playbackToggle]")[0]; // the data tag of the play/pause button is only pause *if* the song is playing
+  return pause.attributes['data-test'].value === "pause";
 }
 
 function pauseMedia() {
-    can_run = true;
-    if (window.pauseKeyLast == undefined) {
-        // do nothing, this is to make sure we don't do the next two
-    } else if ((Date.now() - window.pauseKeyLast) <= 200) {
-        can_run = false;
-    }
-    if (can_run) {
-        log("MusicPaused");
-        document.querySelectorAll("[class^=playbackToggle]")[0].click(); // the pause button is weird, hence the custom filter here
-        window.pauseKeyLast = Date.now();
-    }
-    else {
-        log("RateLimit");
-    }
+  if (window.pauseKeyLast === undefined || !(Date.now() - window.pauseKeyLast) <= 200) {
+    log("MusicPaused");
+    document.querySelectorAll("[class^=playbackToggle]")[0].click(); // the pause button is weird, hence the custom filter here
+    window.pauseKeyLast = Date.now();
+  } else {
+    log("RateLimit");
+  }
 }
 
 function keyDownTextField(e) {
-    var keyCode = e.keyCode;
-    var actualKey = String.fromCharCode(keyCode);
-    var evtobj = window.event ? event : e;
-    if (actualKey == " " && evtobj.shiftKey) {
-        // pauseMedia();
-        // you can uncomment this if for some reason TIDAL doesn't handle it by itself
-    }
-    else if (actualKey == "%" && evtobj.ctrlKey) { // control-left gets inputted as shift-% to our interface
-        previousMedia();
-    }
-    else if (actualKey == "'" && evtobj.ctrlKey) { // control-right gets inputted as shift-' to our interface
-        nextMedia();
-    }
+  let keyCode = e.keyCode;
+  let actualKey = String.fromCharCode(keyCode);
+  let evtObj = window.event ? event : e;
+
+  if (actualKey === " " && evtObj.shiftKey) {
+    // pauseMedia();
+    // you can uncomment this if for some reason TIDAL doesn't handle it by itself
+  } else if (actualKey === "%" && evtObj.ctrlKey) { // control-left gets inputted as ctrl-% to our interface
+    previousMedia();
+  } else if (actualKey === "'" && evtObj.ctrlKey) { // control-right gets inputted as ctrl-' to our interface
+    nextMedia();
+  }
 }
 
-function getMediaInformation() {
-    // we have two footers, both are the same, just the first is the one in the default view
-    footer = document.querySelectorAll("[class^=footerPlayer]")[0];
-    try {
-        mediaInformationDiv = footer.querySelectorAll("[class^=mediaInformation]")[0];
-    }
-    catch (err) {
-        // this happens if for some reason the footer isn't present
-        return window.getMediaInformation();
-    }
-    toReturn = { "title": "Unknown", "artist": "Unknown", "playing_from": "Unknown" };
+function getMediaInformation(callback) {
+  // we have two footers, both are the same, just the first is the one in the default view
+  let toReturn = { "title" : "Unknown", "artist" : "Unknown", "playing_from" : "Unknown" };
+  let footer = document.querySelectorAll("[class^=footerPlayer]")[0];
+  let mediaInformationDiv;
+  try {
+    mediaInformationDiv = footer.querySelectorAll("[class^=mediaInformation]")[0];
+
     // annoyingly, TIDAL don't give us any class for the title, so we just have to iterate over ourselves
-    for (var i = 0; i < mediaInformationDiv.childNodes.length; i++) {
-        if (mediaInformationDiv.childNodes[i].attributes['data-test'] != undefined) {
-            if (mediaInformationDiv.childNodes[i].attributes['data-test'].value == "footer-track-title") {
-                // this means current child has the track title
-                toReturn.title = mediaInformationDiv.childNodes[i].textContent;
-            }
+    for (let i = 0; i < mediaInformationDiv.childNodes.length; i++) {
+      if (mediaInformationDiv.childNodes[i].attributes['data-test'] !== undefined) {
+        if (mediaInformationDiv.childNodes[i].attributes['data-test'].value === "footer-track-title") {
+          // this means current child has the track title
+          toReturn.title = mediaInformationDiv.childNodes[i].textContent;
         }
+      }
     }
+
     toReturn.artist = mediaInformationDiv.querySelectorAll("[class^=mediaArtists]")[0].textContent;
     // this chaos is because TIDAL handily split the player into "Playing from" and the actual playlist
     toReturn.playing_from = mediaInformationDiv.childNodes[2].querySelectorAll("[class^=text]")[0].textContent;
     return toReturn;
+  } catch (err) {
+    // this happens if for some reason the footer isn't present
+    if (typeof callback !== 'function') callback = getMediaInformation;
+    setTimeout(function() {
+      callback()
+    }, 1000);
+    return toReturn;
+  }
 }
 
 function checkMediaNotification() {
-    if (window.previousState == undefined) {
-        window.previousState = getMediaInformation();
-        if (window.previousState == undefined) {
-            // we're still having issues getting track information (hidden footer?)
-            // nothing we can really do, so give up?
-            return;
-        }
+  if (window.previousState === undefined) {
+    window.previousState = getMediaInformation(checkMediaNotification);
+    if (window.previousState === undefined) {
+      // we're still having issues getting track information (hidden footer?)
+      // nothing we can really do, so give up?
+      return;
     }
-    currentState = getMediaInformation();
-    if (currentState.title != window.previousState.title) { // song change has occurred
-        if (isPlaying()) { // we don't want to send a notification if our user isn't listening
-            new Notification(currentState.title, {
-                body: currentState.artist
-            });
-        }
+  }
+
+  let currentState = getMediaInformation();
+
+  if (currentState.title !== window.previousState.title) { // song change has occurred
+    if (isPlaying()) { // we don't want to send a notification if our user isn't listening
+      new Notification(currentState.title, {
+        body : currentState.artist
+      });
     }
-    window.previousState = currentState;
+  }
+  window.previousState = currentState;
 }
 
 function main() {
-    var iDiv = document.createElement('div');
-    iDiv.id = 'log'; // We can't use console.log with injected code
-    iDiv.style = "display: none";
-    iDiv.className = 'block';
-    document.getElementsByTagName('body')[0].appendChild(iDiv);
-    document.addEventListener("keydown", keyDownTextField, false);
-    if (options.enable_notifications) {
-        var notifications = setInterval(checkMediaNotification, 1000);
-    }
-    log("Running");
+  let iDiv = document.createElement('div');
+  iDiv.id = 'log'; // We can't use console.log with injected code
+  iDiv.style = "display: none;";
+  iDiv.className = 'block';
+  document.getElementsByTagName('body')[0].appendChild(iDiv);
+  document.addEventListener("keydown", keyDownTextField, false);
+
+  if (options.enable_notifications) {
+    Notification.requestPermission().then((value) => {
+      if (value === 'default' || value === 'granted') {
+        setInterval(checkMediaNotification, 1000);
+      }
+    });
+  }
+
+  log("Running");
 }
 
 main();
--- main.js
+++ main.js
@@ -10,8 +10,8 @@ var options = {
     asar: false, // we don't need to package our code in an archive
     icon: './icon.png',
     bounce: false,
-    width: 1280,
-    height: 800,
+    width: 1920,
+    height: 1080,
     showMenuBar: false,
     fastQuit: false,
     honest: false,
@@ -21,7 +21,7 @@ var options = {
     clearCache: false,
     tray: true,
     verbose: true,
-    electronVersion: "6.1.0-wvvmp",
+    electronVersion: "12.0.5-wvvmp",
     inject: ["./custom.js"],
     globalShortcuts: "shortcuts.json",
 };
@@ -32,11 +32,11 @@ nativefier(options, function(error, appPath) {
         return;
     }
     // we now need to ensure that nativefier.json is set right (sometimes the name option doesn't pass correctly)
-    var rawdata = fs.readFileSync(appPath+'/resources/app/nativefier.json');  
+    var rawdata = fs.readFileSync(appPath+'/resources/app/nativefier.json');
     var packageData = JSON.parse(rawdata);
     packageData.name = "TIDAL";
     // parts of our README ask people to change this file, so indenting it is polite
-    var to_write = JSON.stringify(packageData, null, 4); 
+    var to_write = JSON.stringify(packageData, null, 4);
     fs.writeFile(appPath+'/resources/app/nativefier.json', to_write, function(error) {
         if(error) throw error;
     });
