f98ba62e6ff1fb59e9b2c7ff8fb8c3894a3184d9
Daniel Fuchs <dfuchs@multamedio.de>
2021-06-25 10:09:03 +0200

    Use async nativefier api & code cleanup & electron version
    - use js imports rather than require
    - use nativefier async api
    - prepare modules
    - updated electron version

--- main.js
+++ main.js
@@ -1,45 +1,32 @@
-const nativefier = require('nativefier').default;
-const fs = require('fs');
-const colors = require('colors/safe');
+import {buildNativefierApp} from 'nativefier';
+import colors from 'colors/safe.js';
 
 // we use nativefier, and just inject code, because that's all we need really
 const options = {
-    name: 'TIDAL',
-    targetUrl: 'https://listen.tidal.com', // required
-    out: 'build/',
-    asar: false, // we don't need to package our code in an archive
-    icon: './icon.png',
-    bounce: false,
-    width: 1920,
-    height: 1080,
-    showMenuBar: false,
-    fastQuit: false,
-    honest: false,
-    conceal: false,
-    zoom: 1.0,
-    singleInstance: true,
-    clearCache: false,
-    tray: true,
-    verbose: true,
-    electronVersion: "12.0.6-wvvmp",
-    inject: ["./custom.js"],
-    globalShortcuts: "shortcuts.json",
+  name: 'TIDAL',
+  targetUrl: 'https://listen.tidal.com', // required
+  out: 'build/',
+  icon: './icon.png',
+  bounce: false,
+  width: 1920,
+  height: 1080,
+  showMenuBar: false,
+  fastQuit: false,
+  honest: false,
+  conceal: false,
+  zoom: 1.0,
+  singleInstance: true,
+  clearCache: false,
+  tray: true,
+  verbose: true,
+  electronVersion: "13.1.4",
+  inject: ["./custom.js"],
+  globalShortcuts: "shortcuts.json",
+  widevine: true,
 };
 
-nativefier(options, function(error, appPath) {
-    if (error) {
-        console.log(colors.red(error));
-        return;
-    }
-
-    // we now need to ensure that nativefier.json is set right (sometimes the name option doesn't pass correctly)
-    const rawData = fs.readFileSync(appPath+'/resources/app/nativefier.json').toString();
-    const packageData = JSON.parse(rawData);
-    packageData.name = "TIDAL";
-    // parts of our README ask people to change this file, so indenting it is polite
-    const data = JSON.stringify(packageData, null, 4);
-    fs.writeFile(appPath+'/resources/app/nativefier.json', data, function(error) {
-        if(error) throw error;
-    });
-    console.log(colors.bold(colors.green('TIDAL is ready to start. cd to "'+appPath+'", and run ./tidal')));
+buildNativefierApp(options).then((appPath) => {
+  console.log(colors.bold(colors.green('TIDAL is ready to start. cd to "'+appPath+'", and run ./TIDAL')));
+}).catch((error) => {
+  console.log(colors.red(error));
 });
